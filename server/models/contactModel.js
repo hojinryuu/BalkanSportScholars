36d807d6500c2c7bf3900bd7d4bb29f418d7f69f
Hojin Ryu <121898510+hojinryuu@users.noreply.github.com>
2024-08-04 19:51:02 -0400

    create model for contacts

--- server/models/contactModel.js
+++ server/models/contactModel.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
+const Schema = mongoose.Schema;
+
 const contactSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  message: { type: String, required: true },
-  date: { type: Date, default: Date.now }
+  message: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Contact', contactSchema);
