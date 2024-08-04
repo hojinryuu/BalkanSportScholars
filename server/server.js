98c828bbdaf2ce998103de6ab1a1c9ba77b1fbcf
Hojin Ryu <121898510+hojinryuu@users.noreply.github.com>
2024-08-04 17:39:33 -0400

    add server.js to initialize Express Server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const dbURI ='mongodb+srv://hojinryu:LJQE7NhnY99z1JnV@primary-cluster.ty6scxp.mongodb.net/?retryWrites=true&w=majority&appName=primary-cluster';
+mongoose.connect(dbURI)
+    .then(() => console.log('MongoDB Atlas connected...'))
+    .catch(err => console.log(err));
+
+// Simple test endpoint
+app.get('/', (req, res) => {
+  res.send('API is working!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
