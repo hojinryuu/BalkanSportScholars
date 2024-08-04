2cecbf96f69ca7bc7112f9530ed4ac439a3886ce
Hojin Ryu <121898510+hojinryuu@users.noreply.github.com>
2024-08-04 18:37:19 -0400

    add public files

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const contactRoutes = require('./routes/contacts');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api/contacts', contactRoutes);
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 const dbURI ='mongodb+srv://hojinryu:LJQE7NhnY99z1JnV@primary-cluster.ty6scxp.mongodb.net/?retryWrites=true&w=majority&appName=primary-cluster';
 mongoose.connect(dbURI)
@@ -18,6 +19,9 @@ mongoose.connect(dbURI)
 app.get('/', (req, res) => {
   res.send('API is working!');
 });
+
+const contactRoutes = require('./routes/contacts');
+app.use('/api/contacts', contactRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
