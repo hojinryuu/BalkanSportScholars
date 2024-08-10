f5b456e2296a4a7834b30e4947aa219e3ce8bb37
Hojin Ryu <121898510+hojinryuu@users.noreply.github.com>
2024-08-10 18:17:11 -0400

    fix minor typo

--- server/models/contactModel.js
+++ server/models/contactModel.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const contactSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  message: { type: String, required: true }
+  message: { type: String, required: true },
+  // date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Contact', contactSchema);
